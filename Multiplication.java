8b640b4a6f121b4c9f202eb92adb966ab661ed2e
LogEman <thelogankessler@gmail.com>
2024-09-30 15:02:51 -0500

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -10,7 +10,7 @@ public class Multiplication {
 
 
         //Default output for result
-        double result = 0;
+        double result = firstNumber * secondNumber;
 
         System.out.print(result);
     }
