517ef5a2df73932fd4f39536105774e534fabe61
4D4850 <84929757+4D4850@users.noreply.github.com>
2024-10-04 02:13:51 +0000

    Added exponentiation to Exponent.java

--- Exponent.java
+++ Exponent.java
@@ -1,9 +1,12 @@
-
 public class Exponent {
     public static void main(String[] args) {
         //Retrieves inputs from the autograder.
         double firstNumber = AutoGraderInput.getFirstNumber();
         double secondNumber = AutoGraderInput.getSecondNumber();
+
+        double result = Math.pow(firstNumber, secondNumber);
+
+        System.out.print(result);
 
     }
 }
