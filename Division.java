c052464583e72c3ad293451b13e7e30b9fc4ccf1
LogEman <thelogankessler@gmail.com>
2024-10-11 02:03:15 +0000

    we made the Division function work

--- Division.java
+++ Division.java
@@ -5,6 +5,10 @@ public class Division {
         double firstNumber = AutoGraderInput.getFirstNumber();
         double secondNumber = AutoGraderInput.getSecondNumber();
 
-
+        // Defines a double variable that takes the variable firstNumber and divides it by the variable secondNumber
+        double quotient = firstNumber / secondNumber;
+        
+        //This function prints the value quotient to the console(´・ω・)シ
+        System.out.print(quotient);
     }
 }
